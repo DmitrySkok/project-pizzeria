1f89ba84d464d6084afaa22dd3815eb445228a0c
Dmitry Skok <dimas.skok95@gmail.com>
2022-01-18 20:59:30 +0100

    Add action for card summary

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,7 @@
       thisCart.products = [];
 
       thisCart.getElements(element);
+      thisCart.initActions();
 
       console.log('new Cart: ', thisCart);
     }
@@ -96,6 +97,16 @@
       thisCart.dom = {};
 
       thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(){
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
     }
   }
 
